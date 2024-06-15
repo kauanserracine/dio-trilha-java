af365803c69a3a38de1553d3c4f7fde14055f9b1
Kauan Serracine <serracinekauan@gmail.com>
2024-06-15 19:43:10 -0300

    Criando scanner no about me

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,13 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class AboutMe {
 
     public static void main(String[] args) {
-        
-        String nome = args [0];
-        String Sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        //Criando o objeto scanner
 
-        System.out.println("Olá me chamo " + nome + " " + Sobrenome);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome : ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Olá me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + "cm");
     }
